12d0b8b938cf0d0ae8761834c152fdd82204035e
adelinegrama <grama.alina2003@gmail.com>
2025-02-27 18:12:38 +0200

    first commit

---
+++ Data/ShopContext.cs
@@ -0,0 +1,20 @@
+using System;
+using Microsoft.EntityFrameworkCore;
+using Shop.Models;
+
+namespace Shop.Data;
+
+public class ShopContext : DbContext
+{
+    public DbSet<Product> Products { get; set; } = null!;
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer("Server=DESKTOP-OFSA363\\SQLEXPRESS;Database=ShopDB;Trusted_Connection=True;");
+    }
+
+    public ShopContext(DbContextOptions<ShopContext> options) : base(options)   
+    {
+
+    }
+}
